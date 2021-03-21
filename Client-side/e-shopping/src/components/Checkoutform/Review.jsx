f2a6e0e2dd3df5ca8715d547cbd763a9683a9509
cdacKPrppss <ss47900986@gmail.com>
2021-03-21 11:39:02 +0530

    NavBar and HandleCartFunction
    Added navigation bar on home page in client side,  also added all the handle cart functions to both sides

---
+++ Client-side/e-shopping/src/components/Checkoutform/Review.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Typography, List, ListItem, ListItemText } from '@material-ui/core';
+
+const Review = ({ caart }) => {
+  
+    return(
+        <>
+            <Typography variant="h6" gutterBottom>Review Order summary</Typography>
+            <List disablePadding>
+                     {caart.cart.map((product) => (
+                        <ListItem style={{ padding: '10px 0' }} key={product.name}>
+                            <ListItemText primary={product.name} secondary={`Quantity: ${product.quantity}`} />
+                            <Typography variant="body2">{product.price}</Typography>
+                        </ListItem>
+                     ))}
+
+                    <ListItem style={{ padding: '10px 0' }}>
+                        <ListItemText primary="Total" />
+                        <Typography variant="subtitle1" style={{ fontWeight: 700 }}>
+                        {caart.totalprice}
+                        </Typography>
+                    </ListItem>
+            </List>
+        </>
+    )
+}
+
+export default Review;
